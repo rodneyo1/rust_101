3c4bcc6e5dbfdb6d82f23318c60b39671b135d5f
rodnochieng <rodneyotieno27@gmail.com>
2025-04-01 14:43:54 +0300

    Initialize scalar package with basic arithmetic functions and configuration files

---
+++ scalar/src/lib.rs
@@ -0,0 +1,19 @@
+pub fn sum (a: i32, b: i32 ) -> i32 {
+    return a + b
+}
+
+pub fn diff (a: i32, b: i32) -> i32 {
+    a - b
+}
+
+pub fn pro (a: i32, b: i32) -> i32 {
+    a * b
+}
+
+pub fn quo (a: f64, b: f64) -> f64 {
+    a / b
+}
+
+pub fn rem (a: f64, b: f64) -> f64 {
+    a % b
+}
