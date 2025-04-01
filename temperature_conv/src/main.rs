b0686ff59555c0c636cac1c38c0f325de5880c1d
rodnochieng <rodneyotieno27@gmail.com>
2025-04-01 15:26:01 +0300

    Improve precision in fahrenheit_to_celsius conversion function

--- temperature_conv/src/main.rs
+++ temperature_conv/src/main.rs
@@ -2,6 +2,5 @@ use temperature_conv::*;
 
 fn main() {
     println!("{} F = {} C", -459.67, fahrenheit_to_celsius(-459.67));
-    println!("{} F = {} C", -459.67, fahrenheit_to_celsius(20.0));
     println!("{} C = {} F", 0.0, celsius_to_fahrenheit(0.0));
 }
