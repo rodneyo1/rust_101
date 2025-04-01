50e90c91c9cc28ca05974863f4c07d255ba8417e
rodnochieng <rodneyotieno27@gmail.com>
2025-04-01 14:50:44 +0300

    Refactor arithmetic functions to handle overflow using checked operations

--- scalar/src/lib.rs
+++ scalar/src/lib.rs
@@ -1,19 +1,19 @@
-pub fn sum (a: i32, b: i32 ) -> i32 {
-    return a + b
+pub fn sum(a: i32, b: i32) -> i32 {
+    a.checked_add(b).expect("attempt to add with overflow")
 }
 
-pub fn diff (a: i32, b: i32) -> i32 {
-    a - b
+pub fn diff(a: i32, b: i32) -> i32 {
+    a.checked_sub(b).expect("attempt to subtract with overflow")
 }
 
-pub fn pro (a: i32, b: i32) -> i32 {
-    a * b
+pub fn pro(a: i32, b: i32) -> i32 {
+    a.checked_mul(b).expect("attempt to multiply with overflow")
 }
 
-pub fn quo (a: f64, b: f64) -> f64 {
+pub fn quo(a: f64, b: f64) -> f64 {
     a / b
 }
 
-pub fn rem (a: f64, b: f64) -> f64 {
+pub fn rem(a: f64, b: f64) -> f64 {
     a % b
 }
