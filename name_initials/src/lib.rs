eb6d48cc79c6f4ce919372458305bcb69a46243b
rodnochieng <rodneyotieno27@gmail.com>
2025-04-03 15:25:15 +0300

    optimize solution

--- name_initials/src/lib.rs
+++ name_initials/src/lib.rs
@@ -1,14 +1,12 @@
 pub fn initials(names: Vec<&str>) -> Vec<String> {
-    let mut result = Vec::with_capacity(names.len());
-    for name in names {
-        let words: Vec<&str> = name.split_whitespace().collect();
-        let word_initials: Vec<String> = words
-            .iter()
-            .filter_map(|word| {
-                word.chars().next().map(|c| format!("{}.", c))
-            })
-            .collect();
-        result.push(word_initials.join(" "));
-    }
-    result
+    names
+        .iter()
+        .map(|name| {
+            name.split_whitespace()
+                .filter_map(|word| word.chars().next())
+                .map(|c| format!("{c}."))
+                .collect::<Vec<_>>()
+                .join(" ")
+        })
+        .collect()
 }
