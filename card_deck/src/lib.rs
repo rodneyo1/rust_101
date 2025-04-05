7365170b92fd405eaa79a603e894318225f1e86b
rodnochieng <rodneyotieno27@gmail.com>
2025-04-05 11:28:34 +0300

    refactor card_deck package to reorganize Suit and Rank enums, enhance random selection methods, and improve code readability

--- card_deck/src/lib.rs
+++ card_deck/src/lib.rs
@@ -1,14 +1,6 @@
 use rand::Rng;
 
-#[derive(Debug, PartialEq)]
-pub enum Suit {
-    Heart,
-    Diamond,
-    Spade,
-    Club,
-}
-
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 pub enum Rank {
     Ace,
     King,
@@ -17,44 +9,54 @@ pub enum Rank {
     Number(u8),
 }
 
+// The `random` function generates a random rank for the card.
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum Suit {
+    Heart,
+    Diamond,
+    Spade,
+    Club,
+}
+
+// The `random` function generates a random suit for the card.
 impl Suit {
-    pub fn random() -> Suit {
-        let mut rng = rand::thread_rng();
-        let value = rng.gen_range(1..=4);
-        Suit::translate(value)
+    pub fn random() -> Self {
+        let value = rand::thread_rng().gen_range(1..5);
+        Self::translate(value)
     }
 
-    pub fn translate(value: u8) -> Suit {
+    pub fn translate(value: u8) -> Self {
         match value {
-            1 => Suit::Heart,
-            2 => Suit::Diamond,
-            3 => Suit::Spade,
-            4 => Suit::Club,
-            _ => panic!("Invalid suit value"),
+            1 => Self::Heart,
+            2 => Self::Diamond,
+            3 => Self::Spade,
+            4 => Self::Club,
+            _ => unreachable!(),
         }
     }
 }
 
+// The `random` function generates a random rank for the card.
 impl Rank {
-    pub fn random() -> Rank {
-        let mut rng = rand::thread_rng();
-        let value = rng.gen_range(1..=13);
-        Rank::translate(value)
+    pub fn random() -> Self {
+        let value = rand::thread_rng().gen_range(1..14);
+        Self::translate(value)
     }
 
-    pub fn translate(value: u8) -> Rank {
+    pub fn translate(value: u8) -> Self {
         match value {
-            1 => Rank::Ace,
-            2..=10 => Rank::Number(value),
-            11 => Rank::Jack,
-            12 => Rank::Queen,
-            13 => Rank::King,
-            _ => panic!("Invalid rank value"),
+            1 => Self::Ace,
+            n @ 2..=10 => Self::Number(n),
+            11 => Self::Jack,
+            12 => Self::Queen,
+            13 => Self::King,
+            _ => unreachable!(),
         }
     }
 }
 
-#[derive(Debug, PartialEq)]
+// The `winner_card` function checks if the given card is the Ace of Spades.
+#[derive(Debug, PartialEq, Clone, Copy)]
 pub struct Card {
     pub suit: Suit,
     pub rank: Rank,
