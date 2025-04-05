c0dbdf551d0131c57faee5789364fe44e5f05628
rodnochieng <rodneyotieno27@gmail.com>
2025-04-05 11:24:14 +0300

    refactor card_deck package to reorder Rank variants, enhance random selection logic, and improve winner_card function comparison

--- card_deck/src/lib.rs
+++ card_deck/src/lib.rs
@@ -11,16 +11,17 @@ pub enum Suit {
 #[derive(Debug, PartialEq)]
 pub enum Rank {
     Ace,
-    Number(u8),
-    Jack,
-    Queen,
     King,
+    Queen,
+    Jack,
+    Number(u8),
 }
 
 impl Suit {
     pub fn random() -> Suit {
-        let random_value = rand::thread_rng().gen_range(1..=4);
-        Suit::translate(random_value)
+        let mut rng = rand::thread_rng();
+        let value = rng.gen_range(1..=4);
+        Suit::translate(value)
     }
 
     pub fn translate(value: u8) -> Suit {
@@ -29,15 +30,16 @@ impl Suit {
             2 => Suit::Diamond,
             3 => Suit::Spade,
             4 => Suit::Club,
-            _ => panic!("Invalid suit value: {}", value),
+            _ => panic!("Invalid suit value"),
         }
     }
 }
 
 impl Rank {
     pub fn random() -> Rank {
-        let random_value = rand::thread_rng().gen_range(1..=13);
-        Rank::translate(random_value)
+        let mut rng = rand::thread_rng();
+        let value = rng.gen_range(1..=13);
+        Rank::translate(value)
     }
 
     pub fn translate(value: u8) -> Rank {
@@ -47,17 +49,20 @@ impl Rank {
             11 => Rank::Jack,
             12 => Rank::Queen,
             13 => Rank::King,
-            _ => panic!("Invalid rank value: {}", value),
+            _ => panic!("Invalid rank value"),
         }
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Card {
     pub suit: Suit,
     pub rank: Rank,
 }
 
 pub fn winner_card(card: Card) -> bool {
-    card.suit == Suit::Spade && matches!(card.rank, Rank::Ace)
+    card == Card {
+        suit: Suit::Spade,
+        rank: Rank::Ace,
+    }
 }
