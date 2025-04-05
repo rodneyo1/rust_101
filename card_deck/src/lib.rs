661a4d66db2a5aaf7807dd78c74d8ad8c005bf65
rodnochieng <rodneyotieno27@gmail.com>
2025-04-05 11:22:04 +0300

    initialize card_deck package with Card, Suit, and Rank structs, and implement random card generation and winner determination functionality

---
+++ card_deck/src/lib.rs
@@ -0,0 +1,63 @@
+use rand::Rng;
+
+#[derive(Debug, PartialEq)]
+pub enum Suit {
+    Heart,
+    Diamond,
+    Spade,
+    Club,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Rank {
+    Ace,
+    Number(u8),
+    Jack,
+    Queen,
+    King,
+}
+
+impl Suit {
+    pub fn random() -> Suit {
+        let random_value = rand::thread_rng().gen_range(1..=4);
+        Suit::translate(random_value)
+    }
+
+    pub fn translate(value: u8) -> Suit {
+        match value {
+            1 => Suit::Heart,
+            2 => Suit::Diamond,
+            3 => Suit::Spade,
+            4 => Suit::Club,
+            _ => panic!("Invalid suit value: {}", value),
+        }
+    }
+}
+
+impl Rank {
+    pub fn random() -> Rank {
+        let random_value = rand::thread_rng().gen_range(1..=13);
+        Rank::translate(random_value)
+    }
+
+    pub fn translate(value: u8) -> Rank {
+        match value {
+            1 => Rank::Ace,
+            2..=10 => Rank::Number(value),
+            11 => Rank::Jack,
+            12 => Rank::Queen,
+            13 => Rank::King,
+            _ => panic!("Invalid rank value: {}", value),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Card {
+    pub suit: Suit,
+    pub rank: Rank,
+}
+
+pub fn winner_card(card: Card) -> bool {
+    card.suit == Suit::Spade && matches!(card.rank, Rank::Ace)
+}
