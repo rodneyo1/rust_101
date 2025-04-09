a0d7886e30be779aa826bc45f8cbf81fea12c55d
rodnochieng <rodneyotieno27@gmail.com>
2025-04-09 12:30:22 +0300

    create middle day function

---
+++ middle_day/src/lib.rs
@@ -0,0 +1,15 @@
+use chrono::{Datelike, NaiveDate, Weekday as wd};
+
+pub fn middle_day(year: usize) -> Option<wd> {
+    let year_i32 = year as i32;  // Convert to i32 for chrono functions
+    let is_leap = NaiveDate::from_ymd_opt(year_i32, 2, 29).is_some();
+    let total_days = if is_leap { 366 } else { 365 };
+
+    if total_days % 2 == 1 {
+        let middle_day_num = total_days / 2 + 1;
+        let date = NaiveDate::from_yo_opt(year_i32, middle_day_num).unwrap();
+        Some(date.weekday())
+    } else {
+        None
+    }
+}
