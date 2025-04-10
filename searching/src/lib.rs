5c49087bd06a6667565c9c44436f3d0d54055840
rodnochieng <rodneyotieno27@gmail.com>
2025-04-10 14:51:26 +0300

    initialize searching crate with search functionality and example usage

---
+++ searching/src/lib.rs
@@ -0,0 +1,8 @@
+pub fn search(array: &[i32], key: i32) -> Option<usize> {
+    for (index, &value) in array.iter().enumerate() {
+        if value == key {
+            return Some(index);
+        }
+    }
+    None
+}
